862fee7712cabd9fb00f4f576746e5fe9ca82dc2
ImaMature <88884623+ImaMature@users.noreply.github.com>
2021-12-05 16:49:56 +0900

    라면 멀티 쓰레드 주석달기

--- Studying/src/Day14/RamenCook.java
+++ Studying/src/Day14/RamenCook.java
@@ -1,46 +1,48 @@
 package Day14;
 
-class RamenCook extends Thread implements Runnable
+class RamenCook extends Thread implements Runnable //쓰레드 상속에 Runnable 인터페이스 선언 //run으로 실행
 {
 	private int ramenCount;
 	private String[] burners = {"_","_","_","_"};
 	
-	public RamenCook(int count)
+	public RamenCook(int count) //RamenProgram클래스에서 num을 인수로 전달받음.
 	{
-		ramenCount = count;
+		ramenCount = count; //ramenCount에 저장
 	}
 	
 	@Override
 	public void run()
 	{
-		while(ramenCount > 0)
+		while(ramenCount > 0) //라면개수가 0이거나 0보다 낮으면 실행X
 		{
-			synchronized(this)
+			synchronized(this) // 한번에 하나의 스레드만 실행 가능 //멀티스레드가 순서대로 실행되도록 하는 것
 			{
-				ramenCount--;
+				ramenCount--; // 라면 개수 줄이기
 				System.out.println(Thread.currentThread().getName() + " : " + ramenCount + "개 남았습니다");
+				//현재 쓰레드의 이름 + 남은 라면 개수 표시
 			}
 			
-			for(int i = 0; i < burners.length; i++)
+			for(int i = 0; i < burners.length; i++) // burners 배열의 길이만큼 반복
 			{
-				if(!burners[i].equals("_"))
+				if(!burners[i].equals("_")) // i번째가 비어있지 않다면
 				{
-					continue;
+					continue; //계속하기
 				}
 				
-				synchronized(this)
+				synchronized(this) 
 				{
-					//if(burners[i].equals("_"))
-					//{
-						burners[i] = Thread.currentThread().getName();
+					//버너가 켜졌을 때 배열에 해당하는 이름을 추가하는 쓰레드
+						burners[i] = Thread.currentThread().getName(); //배열의 i번째에 현재 쓰레드의 이름 저장
 						System.out.println("                 " + Thread.currentThread().getName() + " : [" + (i + 1) + "]번 버너 ON");
+																						//인덱스는 0부터 시작해서  +1 했습니다.
 						showBurners();
-					//}
+						//메소드 호출
+					
 				}
 				
 				try
 				{
-					Thread.sleep(2000);	
+					Thread.sleep(2000);	//2초간 일시정지
 				}
 				catch(Exception e)
 				{
@@ -49,6 +51,7 @@ class RamenCook extends Thread implements Runnable
 				
 				synchronized(this)
 				{
+					//버너를 끈 상태로 보이게 하는 쓰레드 // 순서는 무작위
 					burners[i] = "_";
 					System.out.println("                                  " + Thread.currentThread().getName() + " : [" + (i + 1) + "]번 버너 OFF" );
 					showBurners();
@@ -59,6 +62,9 @@ class RamenCook extends Thread implements Runnable
 			try
 			{
 				Thread.sleep(Math.round(1000 * Math.random()));
+				//지연되는 시간을 0~1초 사이로 무작위로 지정하기 
+				//Math.round = 소수점 이하 반올림
+				//1000*Math.random() = (최댓값 0 ~ 1초)
 			}
 			catch(Exception e)
 			{
@@ -67,14 +73,15 @@ class RamenCook extends Thread implements Runnable
 		}
 	}
 	
+	//버너 배열의 상태를 보여주는 메소드
 	private void showBurners()
 	{
-		String stringToPrint = "                                                             ";
-		for(int i = 0; i < burners.length; i++)
+		String stringToPrint = "                                                             "; // 구별하기 위해 긴 공백 저장
+		for(int i = 0; i < burners.length; i++) // burners 배열의 길이 만큼 반복
 		{
-			stringToPrint += (" " + burners[i]);
+			stringToPrint += (" " + burners[i]); // 배열의 i번째 상태 값 String화해서 stringToPrint에 더해서 저장
 		}
-		System.out.println(stringToPrint);
+		System.out.println(stringToPrint); 
 	}
 
 
--- Studying/src/Day14/RamenProgram.java
+++ Studying/src/Day14/RamenProgram.java
@@ -10,18 +10,19 @@ public class RamenProgram
 
 	public static void main(String[] args) 
 	{
-		int num;
-		Scanner input = new Scanner(System.in);
+		int num; //객체 선언
+		Scanner input = new Scanner(System.in); //라면 개수 입력받기
 		
 		System.out.println("라면 몇 개 끓일까요?");
 		
-		num = input.nextInt();
+		num = input.nextInt(); //객체화
 		
 		System.out.println(num + "개 주문 완료! 조리시작!");
 		try
 		{
-			RamenCook ramenCook = new RamenCook(num);
-			new Thread(ramenCook,"A").start();
+			RamenCook ramenCook = new RamenCook(num); 
+			//입력 받은 num을 RamenCook클래스 생성자에 전달, 그후 ramenCook에 저장해서 객체화
+			new Thread(ramenCook,"A").start(); //(라면갯수,"쓰레드이름") 라면갯수와 쓰레드 이름 지정하기. 쓰레드 시작 run실행
 			new Thread(ramenCook,"B").start();
 			new Thread(ramenCook,"C").start();
 			new Thread(ramenCook,"D").start();
@@ -39,18 +40,19 @@ public class RamenProgram
 
 interface Runnable
 {
-	public void run();
+	public void run(); // RamenCook 클래스의 @override한 run과의 차이를 잘 모르겠습니다.
+	//혹시 RamenCook 클래스에서 오버라이드를 해서 오버라이드한 그 run이 이 친구를 덮어쓰기 한건가요?>>
 }
 
 
 class currentThread extends Thread
 {
-	public RamenCook ramenCook;
-	static String nam;
+	public RamenCook ramenCook; 
+	static String nam; //전역변수로 외부클래스에서 사용가능
 	
 	
-	currentThread(RamenCook ramenCook , String nam)
-	{
+	currentThread(RamenCook ramenCook , String nam) //라면 개수와 쓰레드 이름 전달받는 생성자. 
+	{												// 인스턴스화??
 		this.ramenCook = ramenCook;
 		currentThread.nam = nam;
 	}
