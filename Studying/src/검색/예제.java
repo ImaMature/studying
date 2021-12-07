65cd05af9acb312365704158d51ee76ddccc783d
ImaMature <88884623+ImaMature@users.noreply.github.com>
2021-12-07 23:08:23 +0900

    weaed

--- Studying/src/검색/예제.java
+++ Studying/src/검색/예제.java
@@ -14,8 +14,11 @@ public class 예제 {
 		private int s_num;		//번호
 		private int s_age; 		//나이
 		private double s_height; //키
+		
 		//제일키큰 사람, 제일 번호가 낮은 사람 찾기
-
+		//1번 키큰 사람
+		//2번 나이 많은사람
+		//3번 번호 제일 큰 사람
 		
 		public S_Data(String s_name, int s_num, int s_age, double s_height) {
 			this.s_name = s_name;
@@ -28,17 +31,7 @@ public class 예제 {
 			return s_name + " " + s_num + " " + s_age + " " + s_height;
 		}
 		
-		//키 비교 인터페이스
-		public static final Comparator<S_Data> HEIGHT_ORDER = new HeightOrdercomparator(); 
 		
-		private static class HeightOrdercomparator implements Comparator<S_Data>{
-
-			@Override
-			public int compare(S_Data o1, S_Data o2) {
-				return (o1.s_height>o2.s_height) ? 1:
-						(o1.s_height < o2.s_height) ? -1 : 0;
-		
-		}
 		public static void main(String[] args) {
 			Scanner sc = new Scanner(System.in);
 			S_Data [] a = {	new S_Data("홍길동", 1, 25, 167.2),
@@ -49,21 +42,21 @@ public class 예제 {
 							new S_Data("신기루", 6, 23, 178.7),
 							new S_Data("한길만", 7, 24, 162.4),
 			};
-		
 			
 			
-			double i = Arrays.binarySearch(
-					a,
-					new S_Data("", 0, 0, s_height),
-					S_Data.HEIGHT_ORDER
-			);
+			String [] s = a.toString().split("\n");	
 			
-			if(i<0.0) {
-				System.out.println("해당하는 값이 없습니다.");
+			System.out.println("1. 가장 키가 큰 사람 2. 가장 키가 작은 사람");
+			int ch = sc.nextInt();
+			if(ch==1) {
 				
+			}else if (ch==2) {
+			
 			}else {
-				System.out.println("배열 ["+i+"]번째 에 있습니다.");
+				System.out.println("잘못된 입력");
 			}
+			
+			
 			
 		}
 	}
